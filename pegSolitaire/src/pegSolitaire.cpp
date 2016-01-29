298d87f3721441c0425f184a263e16a61eb12f90
Vittorio Mojoli <vittorio.mojoli@gmail.com>
2016-01-29 19:04:16 +0000

    Peg Solitaire main scheleton

---
+++ pegSolitaire/src/pegSolitaire.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : pegSolitaire.cpp
+// Author      : Vittorio Mojoli
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Welcome to Peg Solitaire" << endl; // prints !!!Hello World!!!
+	return 0;
+}
